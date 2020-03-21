23a866e93242fdd3b07809321d3eb2960a3c2299
tongjiarong <937557708@qq.com>
2020-03-21 21:57:42 +0800

    test

--- services/spring-boot-dome-1/src/main/java/com/tjr/common/SystemConfig.java
+++ services/spring-boot-dome-1/src/main/java/com/tjr/common/SystemConfig.java
@@ -1,18 +1,22 @@
 package com.tjr.common;
 
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tjr.Entity.SysResources;
 import com.tjr.Entity.SysRole;
 import com.tjr.Entity.SysUser;
 import com.tjr.dao.SysUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * 重启 赋值 初始化
+ */
 @Component
 public class SystemConfig {
 
@@ -33,6 +37,17 @@ public class SystemConfig {
 			List<SysResources> sysResources=userRepository.getUserSysResourcesList(userId);
 			sysUser.setSysResources(sysResources);;
 		}
+	}
+
+	@Autowired
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public static JPAQueryFactory queryFactory;
+
+	@PostConstruct
+	public void init() {
+		queryFactory = new JPAQueryFactory(entityManager);
 	}
 
 }
--- services/spring-boot-dome-1/src/main/java/com/tjr/controller/PublicAllController.java
+++ services/spring-boot-dome-1/src/main/java/com/tjr/controller/PublicAllController.java
@@ -3,11 +3,14 @@ package com.tjr.controller;
 import com.querydsl.core.types.Predicate;
 import com.tjr.Entity.QTSysMenu;
 import com.tjr.Entity.TSysMenu;
+import com.tjr.common.SystemConfig;
 import com.tjr.dao.TSysMenuRepository;
+import com.tjr.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,12 +18,31 @@ public class PublicAllController {
     @Autowired
     private TSysMenuRepository iTSysMenuReository;
 
-    @GetMapping("/jpa")
-    public void testJpa(){
-        QTSysMenu qc=QTSysMenu.tSysMenu;
-        Predicate pre=qc.url.like("%system%");
-        List<TSysMenu> list=  (List<TSysMenu>) iTSysMenuReository.findAll(pre);
-        System.out.println(list.size());
 
+
+
+    @GetMapping("/jpa")
+    public ResponseUtils testJpa() {
+        QTSysMenu qc = QTSysMenu.tSysMenu;
+        Predicate pre = qc.url.like("%system%");
+
+        List<TSysMenu> list = (List<TSysMenu>) iTSysMenuReository.findAll(pre);
+        System.out.println(list.size());
+        return ResponseUtils.ok(list);
+    }
+
+
+    @GetMapping("/jpa1")
+    public ResponseUtils testJpa1() {
+        QTSysMenu qc = QTSysMenu.tSysMenu;
+        Predicate pre = qc.url.like("%system%");
+
+        List<TSysMenu> list=new ArrayList<>();
+        try{
+            list = SystemConfig.queryFactory.selectFrom(qc).where(pre).fetch();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return ResponseUtils.ok(list);
     }
 }
