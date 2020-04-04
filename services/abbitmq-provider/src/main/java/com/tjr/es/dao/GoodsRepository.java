ade605d83b834397644fd949bf4a06df9d321c3a
tongjiarong <937557708@qq.com>
2020-04-04 17:30:49 +0800

    ss

--- services/abbitmq-provider/src/main/java/com/tjr/es/dao/GoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tjr.es.dao;
-
-import com.tjr.es.model.Goods;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface GoodsRepository extends ElasticsearchRepository<Goods,Long> {
-}
